d00df032592c5cb27b5f18e9ad06248e92f1ab47
crouchcd <cameroncrouch36@gmail.com>
2023-02-09 17:06:40 -0600

    multiline input not working as expected

--- .github/actions/pr-checklist/dist/index.js
+++ .github/actions/pr-checklist/dist/index.js
@@ -2821,11 +2821,12 @@ const core = __nccwpck_require__(186);
 // const github = require("@actions/github");
 
 try {
-  const filesChanged = core.getMultilineInput("files-changed");
-  if (filesChanged.length <= 1) {
+  const filesChanged = core.getInput("files-changed");
+  const splitby__I__ = filesChanged.split("__I__");
+  if (splitby__I__.length <= 1) {
     process.exit(1);
   }
-  for (const file of filesChanged) {
+  for (const file of splitby__I__) {
     console.log("__file__:", file);
   }
 } catch (error) {
