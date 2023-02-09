eb879b7b9981def95df4a8c93758cb049f199470
crouchcd <cameroncrouch36@gmail.com>
2023-02-09 17:01:21 -0600

    multiline test (2)

--- .github/actions/pr-checklist/dist/index.js
+++ .github/actions/pr-checklist/dist/index.js
@@ -2821,9 +2821,12 @@ const core = __nccwpck_require__(186);
 // const github = require("@actions/github");
 
 try {
-  const filesChanged = core.getInput("files-changed");
-  for (const file of filesChanged.split("__I__")) {
-    console.log("FILE:", file);
+  const filesChanged = core.getMultilineInput("files-changed");
+  if (filesChanged.length <= 1) {
+    process.exit(1);
+  }
+  for (const file of filesChanged) {
+    console.log("__file__:", file);
   }
 } catch (error) {
   core.setFailed(error.message);
--- .github/actions/pr-checklist/index.js
+++ .github/actions/pr-checklist/index.js
@@ -2,9 +2,12 @@ const core = require("@actions/core");
 // const github = require("@actions/github");
 
 try {
-  const filesChanged = core.getInput("files-changed");
-  for (const file of filesChanged.split("__I__")) {
-    console.log("FILE:", file);
+  const filesChanged = core.getMultilineInput("files-changed");
+  if (filesChanged.length <= 1) {
+    process.exit(1);
+  }
+  for (const file of filesChanged) {
+    console.log("__file__:", file);
   }
 } catch (error) {
   core.setFailed(error.message);
